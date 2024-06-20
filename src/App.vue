0c3b16db715103bcad11fbf51a12a136e5e55ad1
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-20 12:32:19 +0530

    didthe implementation and styling for the timer

--- src/App.vue
+++ src/App.vue
@@ -28,46 +28,5 @@ export default {
   background: #fff8ec;
 }
 
-.container {
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-}
-
-.title {
-  margin-top: 50px;
-}
-
-h1 {
-  font-size: 64px;
-  color: #c44747;
-  margin-right: 10px;
-}
-
-.timer {
-  position: relative;
-  margin-top: 100px;
-  width: 330px;
-  height: 330px;
-}
-
-button{
-  margin-top: 50px;
-  width: 200px;
-  height: 60px;
-  background: #f85959;
-  border-radius: 20px;
-  border: none;
-  cursor: pointer;
-  font-size: 36px;
-  line-height: 45px;
-  text-align: center;
-  color: #fff8ec;
-}
-
-button:focus{
-  outline: none;
-}
 
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,180 @@
 <template>
-    <div class="container">
-        <div class="title">
-            <h1>Pomodoro 🍅</h1>
-        </div>
-        <div class="timer"></div>
-        <button>Start</button>
+  <div class="container">
+    <div class="title">
+      <h1>Pomodoro 🍅</h1>
     </div>
-  
+    <div class="timer">
+      <svg
+        width="163"
+        height="165"
+        viewBox="-8 -8 184 188"
+        fill="none"
+        id="first-segment"
+      >
+        <path
+          stroke="#E9D2B1"
+          stroke-width="15"
+          stroke-linecap="round"
+          d="M165.16,163.38A172,172,0,0,0,0,0"
+        />
+        <path
+          id="top-right"
+          stroke="#F85959"
+          stroke-width="15"
+          stroke-linecap="round"
+          d="M165.16,163.38A172,172,0,0,0,0,0"
+        />
+      </svg>
+      <svg
+        width="163"
+        height="165"
+        viewBox="-8 -8 184 188"
+        fill="none"
+        id="second-segment"
+      >
+        <path
+          stroke="#E9D2B1"
+          stroke-width="15"
+          stroke-linecap="round"
+          d="M0,163.34A172,172,0,0,0,164.44,0"
+        />
+        <path
+          id="bottom-right"
+          stroke="#F85959"
+          stroke-width="15"
+          stroke-linecap="round"
+          d="M0,163.34A172,172,0,0,0,164.44,0"
+        />
+      </svg>
+      <svg
+        width="163"
+        height="165"
+        viewBox="-8 -8 184 188"
+        fill="none"
+        id="third-segment"
+      >
+        <path
+          stroke="#E9D2B1"
+          stroke-width="15"
+          stroke-linecap="round"
+          d="M0,0A172,172,0,0,0,165.16,162.61"
+        />
+        <path
+          id="bottom-left"
+          stroke="#F85959"
+          stroke-width="15"
+          stroke-linecap="round"
+          d="M0,0A172,172,0,0,0,165.16,162.61"
+        />
+      </svg>
+      <svg
+        width="163"
+        height="165"
+        viewBox="-8 -8 184 188"
+        fill="none"
+        id="fourth-segment"
+      >
+        <path
+          stroke="#E9D2B1"
+          stroke-width="15"
+          stroke-linecap="round"
+          d="M160.17,0A172,172,0,0,0,0,161.51"
+        />
+        <path
+          id="top-left"
+          stroke="#F85959"
+          stroke-width="15"
+          stroke-linecap="round"
+          d="M160.17,0A172,172,0,0,0,0,161.51"
+        />
+      </svg>
+      <h2>25.00</h2>
+    </div>
+    <button>Start</button>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'HomeVue'
-
-}
+  name: "HomeVue",
+};
 </script>
 
 <style>
+.container {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.title {
+  margin-top: 50px;
+}
+
+h1 {
+  font-size: 64px;
+  color: #c44747;
+  margin-right: 10px;
+}
+
+.timer {
+  position: relative;
+  margin-top: 100px;
+  width: 330px;
+  height: 330px;
+}
+
+#first-segment{
+    position: absolute;
+    top: 0;
+    right: 0;
+}
+#second-segment{
+    position: absolute;
+    bottom: 0;
+    right: 0;
+}
+
+#third-segment{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+}
+
+
+#fourth-segment{
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+
+h2{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    font-size: 64px;
+    color:#f85959;
+}
+
+
+button {
+  margin-top: 50px;
+  width: 200px;
+  height: 60px;
+  background: #f85959;
+  border-radius: 20px;
+  border: none;
+  cursor: pointer;
+  font-size: 36px;
+  line-height: 45px;
+  text-align: center;
+  color: #fff8ec;
+}
+
+button:focus {
+  outline: none;
+}
 
 </style>
