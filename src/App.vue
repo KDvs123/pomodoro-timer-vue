4b9ea7c7ddc65c60a4286142a1e02f7399f0236e
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-20 12:11:58 +0530

    created the template in vue, import lobster font and added the global styling

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>Test</h1>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Lobster',cursive;
+  color: #f85959;
+}
+
+#app {
+  
+}
+</style>
